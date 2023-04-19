ee59b6f307c1afeb9b15abfa97a76600c66a3718
2806710132 <2806710132@qq.com>
2023-04-19 21:32:57 +0800

    test_2_change

--- test/src/gat_firest.java
+++ test/src/gat_firest.java
@@ -2,6 +2,7 @@ public class gat_firest {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("添加dev修改");
+        System.out.println("test_2");
         System.out.println("test_3");
         System.out.println("test_2");
     }
